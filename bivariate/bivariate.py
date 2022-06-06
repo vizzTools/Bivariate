f4fcfef02e818074d356cb9990033795b17bfc2c
Biel Stela <biel.stela@gmail.com>
2022-06-06 09:10:19 +0200

    Improve docstring and add tests!

--- bivariate/bivariate.py
+++ bivariate/bivariate.py
@@ -24,6 +24,15 @@ class BivariateColorMap:
     def _make_labels(self) -> tuple[list[str], list[str], list[str]]:
         """Makes a list of labels for the bivariate color map of length n_rows**2.
         The labels are a combination of the letters A-Z and the numbers 1-9.
+
+        Returns
+        -------
+        letters: list
+            Letter codes for first variable
+        numbers: list
+            Numeric codes for second variable
+        labels: list
+            result of cartesian product between letters and numbers
         """
         letters = list(string.ascii_uppercase[: self.n_rows])
         numbers = [str(n) for n in range(1, self.n_rows + 1)]
@@ -33,7 +42,7 @@ class BivariateColorMap:
             ["".join([*pair]) for pair in product(letters, numbers)],
         )
 
-    def sorted_labels(self, reverse: bool = True) -> list[str]:
+    def _sorted_labels(self, reverse: bool = True) -> list[str]:
         """Sorted labels by number
 
         Used to match the order of the colors produced by
@@ -43,8 +52,15 @@ class BivariateColorMap:
         return sorted(self.labels, key=lambda x: x[1], reverse=reverse)
 
     def plot_color_square(self, show_labels=True) -> plt.Axes:
-        """Plots the square of colors with the corresponding labels."""
-        labels = np.array(self.sorted_labels()).reshape(self.n_rows, self.n_rows)
+        """Plots the square of colors with the corresponding labels.
+
+        Parameters
+        ----------
+        show_labels: bool
+            Show category labels on each color
+        """
+        labels = np.array(self._sorted_labels()).reshape(self.n_rows, self.n_rows)
+        # color array must be rotated so the lighter color is at the lower left corner
         rgba_colors = np.rot90(to_rgba_array(self.colors).reshape(self.n_rows, self.n_rows, 4))
 
         _, ax = plt.subplots()
@@ -56,7 +72,7 @@ class BivariateColorMap:
 
     def cmap(self) -> dict[str:str]:
         """Returns a map of labels to colors."""
-        return dict(zip(self.sorted_labels(reverse=False), self.colors))
+        return dict(zip(self._sorted_labels(reverse=False), self.colors))
 
     def classify(self, var1: pd.Series, var2: pd.Series) -> pd.Series:
         """Classifies the data using the bivariate color map."""
--- tests/test_bivariate.py
+++ tests/test_bivariate.py
@@ -5,18 +5,39 @@
 import pytest
 from bivariate import bivariate
 
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+COLORS_9 = ["#e8e8e8", "#a6d9d9", "#5ac8c8", "#d3a7cb", "#a6a7cb", "#5aa7c8", "#be64ac", "#a664ac", "#5a64ac"]
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+class TestBivariateColorMap:
+    def test_only_takes_square_number_of_colors(self):
+        bad_length_colors = COLORS_9 + ["#ffffff"]  # 10 colors is not a square number
+        with pytest.raises(ValueError) as exc:
+            bivariate.BivariateColorMap(bad_length_colors)
+        assert "The number of colors must be a square." in str(exc.value)
+
+    def test_make_labels(self):
+        bivar = bivariate.BivariateColorMap(COLORS_9)
+
+        assert bivar.n_rows == 3
+        assert bivar.label_letters == ["A", "B", "C"]
+        assert bivar.label_numbers == ["1", "2", "3"]
+        assert bivar.labels == "A1 A2 A3 B1 B2 B3 C1 C2 C3".split()
+
+    def test_labels_sorted(self):
+        bivar = bivariate.BivariateColorMap(COLORS_9)
+        assert bivar._sorted_labels() == "A3 B3 C3 A2 B2 C2 A1 B1 C1".split()
+
+    def test_cmap(self):
+        bivar = bivariate.BivariateColorMap(COLORS_9)
+        expected_cmap = {
+            "A1": "#e8e8e8",
+            "B1": "#a6d9d9",
+            "C1": "#5ac8c8",
+            "A2": "#d3a7cb",
+            "B2": "#a6a7cb",
+            "C2": "#5aa7c8",
+            "A3": "#be64ac",
+            "B3": "#a664ac",
+            "C3": "#5a64ac",
+        }
+        assert bivar.cmap() == expected_cmap
