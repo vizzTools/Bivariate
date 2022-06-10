7d60f7cd487294afce1c5737f45e17bf2dde1329
Biel Stela <biel.stela@gmail.com>
2022-06-10 07:55:37 +0200

    Updates deps and makes main class importable at module top level

--- bivariate/__init__.py
+++ bivariate/__init__.py
@@ -1,5 +1,7 @@
 """Top-level package for bivariate."""
 
+from bivariate.bivariate import BivariateColorMap  # noqa: F401
+
 __author__ = """Biel Stela"""
 __email__ = "biel.stela@vizzuality.com"
 __version__ = "0.1.0"
